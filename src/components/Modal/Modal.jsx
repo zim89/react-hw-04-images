d0be51eb87780aff3563704e7f89d929adf3411b
hadgi89 <71321662+hadgi89@users.noreply.github.com>
2023-04-29 20:38:47 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -12,7 +12,7 @@ function Modal({ onCloseModal, largeImage, tags }) {
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, []);
+  });
 
   const handleKeyDown = e => {
     if (e.code === 'Escape') {
@@ -43,3 +43,46 @@ Modal.propTypes = {
 };
 
 export default Modal;
+
+// const modalRoot = document.querySelector('#modal-root');
+
+// export default class Modal extends Component {
+//   static propTypes = {
+//     onCloseModal: PropTypes.func.isRequired,
+//     largeImage: PropTypes.string.isRequired,
+//     tags: PropTypes.string.isRequired,
+//   };
+
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onCloseModal();
+//     }
+//   };
+
+//   handleBackdropClick = e => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onCloseModal();
+//     }
+//   };
+
+//   render() {
+//     const { largeImage, tags } = this.props;
+
+//     return createPortal(
+//       <div className="overlay" onClick={this.handleBackdropClick}>
+//         <div className="modal">
+//           <img src={largeImage} alt={tags} />
+//         </div>
+//       </div>,
+//       modalRoot
+//     );
+//   }
+// }
